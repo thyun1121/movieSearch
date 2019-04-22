47d33eaf2726a5521b73b43a7a4644df2a3e0786
david <thyun.ahn@gmail.com>
2019-04-22 20:20:20 +0900

    first commit to bitbucket

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true })); //app.use bodyParser 왜 쓰는지?
 
 const mysql = require("mysql");
 
